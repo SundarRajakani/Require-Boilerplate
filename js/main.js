9d87bc11467ff6c3c54fe631e49c7e4f45ac0bd0
John Cockrell <sjhcockrell@gmail.com>
2012-09-04 19:30:22 -0400

    Update library code

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
 
 requirejs.config({
     paths: {
-        jquery: 'libs/jquery-1.7.2.min',
+        jquery: 'libs/jquery-1.8.1.min',
         underscore: 'libs/underscore-1.3.3.min',
         backbone: 'libs/backbone-0.9.2.min',
         console: 'submodules/safe-console/console',
