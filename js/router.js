fbfbb790b2b779926bd466588c97fc20f1a21b80
John Cockrell <sjhcockrell@gmail.com>
2012-09-21 16:19:40 -0400

    Add link interpretation into boilerplate

--- js/router.js
+++ js/router.js
@@ -11,15 +11,43 @@ define([
     var AppRouter = Backbone.Router.extend({
         initialize: function( frame ){
             this.frame = frame;
+
+            //  Default Link Handling:
+            //  +   Any anchor with an HREF in front of it, we're going to assume
+            //      is an outside resource.
+            //  +   Anything else, feed through the router.
+            //  +   Also handily prevents JS injection.
+            $(document).on("click touchend", "a:not([data-bypass])", function( ev ){
+                var clicked = this;
+                var href = $(clicked).attr('href');
+                var protocol = clicked.protocol + '//';
+
+                if (href && href.slice(0, protocol.length) !== protocol &&
+                    href.indexOf("javascript:") !== 0) {
+
+                    C.log('continuing');
+
+                    //  Prevent reload.
+                    ev.preventDefault();
+
+                    //  Add into the backbone history stack.
+                    Backbone.history.navigate(href, true);
+
+                } else {
+                    //  Trace for outbound links if we ever want to track.
+                    C.log('outbound');
+                }
+            });
+
         },
         routes: { 
-            '': 'defaultRoute',
+            '': 'index',
             /* add routes here */
         },
        
         //  Landing page.
-        defaultRoute: function(){
-            C.log('Route:default');
+        index: function(){
+            C.log('Route:index');
             var self = this;
 
             var landingPage = new Pages.LandingPage({ router: self }).render();
